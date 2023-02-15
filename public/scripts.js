676a33883a68a483807ca26bda35f24e2c0276a1
krisdurkin <kristopherdurkin@outlook.com>
2023-02-15 14:49:52 +0000

    styling

--- public/scripts.js
+++ public/scripts.js
@@ -35,3 +35,14 @@ document.getElementById("cards").onmousemove = e => {
     card.style.setProperty("--mouse-y", `${y}px`);
   };
 }
+
+document.getElementById("cards").onmousemove = e => {
+  for(const card of document.getElementsByClassName("work-grid-item")) {
+    const rect = card.getBoundingClientRect(),
+          x = e.clientX - rect.left,
+          y = e.clientY - rect.top;
+
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  };
+}
