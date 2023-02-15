e647299e5a4fff889fae4b1e09448682ce64e81d
krisdurkin <kristopherdurkin@outlook.com>
2023-02-15 10:23:50 +0000

    fixed admin login

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/scripts.js');
     eleventyConfig.addPassthroughCopy('./src/media');
     eleventyConfig.addPassthroughCopy('./src/fonts');
+    eleventyConfig.addPassthroughCopy('./src/admin');
 
     return{
         dir: {
